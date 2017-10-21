ffeee491b7dc07de93ec458b09903cbc4dfdc7e9
Tu nombre aquí“$ git config --global user.name Manrique“ <bmanrique17@unitec.edu>
2017-10-20 20:29:04 -0600

    tenia un error, ahora si estan las clases

--- Lab#2_BryanAmador/src/lab/pkg2_bryanamador/Lab2_BryanAmador.java
+++ Lab#2_BryanAmador/src/lab/pkg2_bryanamador/Lab2_BryanAmador.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab.pkg2_bryanamador;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class Lab2_BryanAmador {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        char opcion;
+        do{
+            System.out.print("******HARVARD******\n"
+                    + "(1)Administracion\n"
+                    + "(2)Matricula\n"
+                    + "(3)LogIn\n"
+                    + "(4)Salir\n\n"
+                    + "Ingrese su opcion:"
+                    );
+            opcion = sc.next().charAt(0);
+            while(opcion!= '1' && opcion!= '2' && opcion!= '3' &&
+                opcion!= '4'){
+                System.out.println("Opcion invalida, de nuevo:");
+                opcion = sc.next().charAt(0);
+            }
+            switch(opcion){
+                case '1':
+                    Administracion();
+                    break;
+                case '2':
+                    Matricula();
+                    break;
+                case '3':
+                    LogIn();
+                    break; 
+            }
+        }while(opcion!= '4');
+        
+    }
+    
+    public static void Administracion(){
+        
+    }
+    
+    public static void Matricula(){
+        
+    }
+    
+    public static void LogIn(){
+        
     }
     
 }
